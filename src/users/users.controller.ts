d004d1e49b63c1e92cfe20f6c7c5cbf67414d383
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-01 14:14:53 +0100

    Changed to use on class instead of method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,15 +15,11 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
-  }
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
@@ -36,6 +32,11 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
   }
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
