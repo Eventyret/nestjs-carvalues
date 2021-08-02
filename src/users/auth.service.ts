5dca39a7508bc4ac08da764c64e4ac14f14bac51
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-02 22:04:10 +0100

    Added hash and salt function

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+  async signup(email: string, password: string) {
+    // Check if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException(`Email ${email} is already in use.`);
+    }
+    // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed results and the salt together
+    const result = salt + '.' + hash.toString('hex');
+
+    // Create a new user and save it to the database
+
+    // return the user
+  }
+
+  signin() {}
 }
