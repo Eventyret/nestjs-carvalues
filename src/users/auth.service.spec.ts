e091e73c9654056295c698f94b3d7f5dd8301aca
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-04 09:28:53 +0100

    Added auth test and fixed jest speed

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,21 +1,26 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    // Fake copy of user service
+    const fakeUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
+    }).compile();
 
-it('can create an instance of auth service', async () => {
-  // Fake copy of user service
-  const fakeUserService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUserService },
-    ],
-  }).compile();
-
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+    service = module.get<AuthService>(AuthService);
+  });
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+  it('can authenticate user', async () => {});
 });
